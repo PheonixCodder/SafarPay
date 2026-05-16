1e67b835355f39788b222e29d0f2e7bc2714e2bd
Ubaidullah Ismail <abdulahadismail.dev@gmail.com>
2026-05-15 23:33:49 -0700

    Add home image asset constants

--- client/lib/utils/constants/images.dart
+++ client/lib/utils/constants/images.dart
@@ -11,4 +11,18 @@ class SImages {
   static const String onBoarding1 = "assets/images/onboarding/ON1.jpg";
   static const String onBoarding2 = "assets/images/onboarding/ON2.jpg";
   static const String onBoarding3 = "assets/images/onboarding/ON3.jpg";
+
+  // -- Banners
+  static const String banner1 = "assets/images/banners/banner1.png";
+  static const String banner2 = "assets/images/banners/banner2.png";
+
+  // -- Categories
+  static const String groceries = "assets/images/categories/groceries.png";
+  static const String cityRides = "assets/images/categories/city-rides.png";
+  static const String freight = "assets/images/categories/freight.png";
+  static const String courier = "assets/images/categories/courier.png";
+  static const String cityToCity = "assets/images/categories/city-to-city.png";
+
+  // -- User
+  static const String user = "assets/images/banners/banner1.png";
 }
